277b225821ca9796026a4d691009e77e26b761d8
Kate Temkin <k@ktemkin.com>
2020-01-19 04:52:57 -0700

    gateware.applets: fix dc_flash applet; does not fix #1

--- luna/gateware/applets/dc_flash.py
+++ luna/gateware/applets/dc_flash.py
@@ -8,7 +8,7 @@ from functools import reduce
 from nmigen import Signal, Elaboratable, Module, Cat, ClockDomain, ClockSignal, ResetInserter
 from nmigen.lib.cdc import FFSynchronizer
 
-from luna.gateware.platform        import get_appropriate_platform
+from luna.gateware.utils.cdc       import synchronize
 from luna.gateware.interface.spi   import SPIRegisterInterface
 from luna.gateware.interface.ulpi  import UMTITranslator
 from luna.gateware.interface.flash import ECP5ConfigurationFlashInterface
@@ -48,41 +48,31 @@ class DebugControllerFlashBridge(Elaboratable):
         spi_flash_bus = platform.request('spi_flash')
         spi_flash_passthrough = ECP5ConfigurationFlashInterface(bus=spi_flash_bus)
 
-        m.submodules += spi_flash_passthrough 
+        m.submodules += spi_flash_passthrough
         m.d.comb += [
             spi_flash_passthrough.sck   .eq(board_spi.sck),
             spi_flash_passthrough.sdi   .eq(board_spi.sdi),
             flash_sdo                   .eq(spi_flash_passthrough.sdo),
         ]
 
-
         #
         # Structural connections.
         #
-        sck = Signal()
-        sdi = Signal()
-        cs  = Signal()
-        gateware_sdo = Signal()
-
-        #
-        # Synchronize each of our I/O SPI signals, where necessary.
-        #
-        m.submodules += FFSynchronizer(board_spi.sck, sck)
-        m.submodules += FFSynchronizer(board_spi.sdi, sdi)
-        m.submodules += FFSynchronizer(board_spi.cs,  cs)
+        spi = synchronize(m, board_spi)
 
         # Select the passthrough or gateware SPI based on our chip-select values.
-        with m.If(spi_registers.cs):
+        gateware_sdo = Signal()
+        with m.If(spi_registers.spi.cs):
             m.d.comb += board_spi.sdo.eq(gateware_sdo)
         with m.Else():
             m.d.comb += board_spi.sdo.eq(flash_sdo)
 
         # Connect our register interface to our board SPI.
         m.d.comb += [
-            spi_registers.sck .eq(sck),
-            spi_registers.sdi .eq(sdi),
-            gateware_sdo      .eq(spi_registers.sdo),
-            spi_registers.cs  .eq(cs)
+            spi_registers.spi.sck .eq(spi.sck),
+            spi_registers.spi.sdi .eq(spi.sdi),
+            gateware_sdo          .eq(spi_registers.spi.sdo),
+            spi_registers.spi.cs  .eq(spi.cs)
         ]
 
         return m
