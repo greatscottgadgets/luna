db7d691e0eda504de7d8a84a525d1fa6aa4592ec
Tom Keddie <git@bronwenandtom.com>
2021-04-28 06:00:25 -0700

    examples/blinky_verilog: updates to match current hw & blinky.py

--- examples/blinky_verilog/blinky.v
+++ examples/blinky_verilog/blinky.v
@@ -5,15 +5,16 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-module top(input clk_60mhz, output [5:0] led, output [3:0] user_io);
+module top(input clk_60mhz, output [5:0] led, output [1:0] user_io);
 
-	 reg [24:0] div = 0;
+	 reg [26:0] div = 0;
 
 	 always @ ( posedge clk_60mhz ) begin
 	 	div <= div + 1'b1;
 	 end
 
-	 assign led = div[24:19];
-	 assign user_io = div[24:21];
+     // we invert here as the leds are pull down for on
+	 assign led = ~div[26:21];
+	 assign user_io = div[25:24];
 
 endmodule
