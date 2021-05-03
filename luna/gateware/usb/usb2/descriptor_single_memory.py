4324d2f1d1e637f045b1fd0bdf8ce7ff0160df50
Tobias Müller <Tobias_Mueller@twam.info>
2021-05-03 21:33:56 +0200

    Support more descriptor types

--- luna/gateware/usb/usb2/descriptor_single_memory.py
+++ luna/gateware/usb/usb2/descriptor_single_memory.py
@@ -66,44 +66,69 @@ class GetDescriptorHandlerSingleMemory(Elaboratable):
         self.stall          = Signal()
 
     def generate_rom_content(self):
-        """ Generates the ROM content. """
+        """ Generates the ROM content.
 
-        # Memory layout
-        # All data is aligned on 4 bytes
-        #
-        # Type offsets and number of entries
-        # 0000  0 (unused)
-        # 0002  0 (unused)
-        # 0004  Number of device indexes
-        # 0006  Address of first device index
-        # 0008  Number of configuration indexes
-        # 000A  Address of first configuration index
-        # 000C  Number of string indexes
-        # 000E  Address of first string index
-        #
-        # Index offsets and length of descriptor
-        # 0010  Length of first device descriptor
-        # 0012  Address of first device descriptor
-        # ...
-        #
-        # Data
-        # ...   Descriptor data
+        Memory layout
+        -------------
+
+        All data is aligned on 4 bytes
+
+        Type offsets and number of entries
+        ----------------------------------
+
+        Each descriptor type starting from 0 until maximum used type number has
+        an entry consisting of number of indexes for this type number (2 bytes)
+        and address of first index (2 bytes).
+        Invalid entries have a value of 0xFFFFFFFF
+
+        Example:
+        0000  0xFFFF
+        0002  0xFFFF
+        0004  Number of device indexes
+        0006  Address of first device index
+        0008  Number of configuration indexes
+        000A  Address of first configuration index
+        000C  Number of string indexes
+        000E  Address of first string index
+        ...
+
+        Index offsets and length of descriptor
+        --------------------------------------
+        Each index of a descriptor type has an entry consistent of the length
+        of the descriptor (2 bytes) and the address of first data byte (2 bytes).
+
+        0010  Length of first device descriptor
+        0012  Address of first device descriptor
+        ...
+
+        Data
+        ----
+        Descriptor data for each descriptor. Padded by 0 to next 4-byte address.
+
+        ...   Descriptor data
+
+        """
 
         # Get all descriptors and cache them in a dictionary, so that we can access them at will
         descriptors = {
-            StandardDescriptorNumbers.DEVICE: {},
-            StandardDescriptorNumbers.CONFIGURATION: {},
-            StandardDescriptorNumbers.STRING: {}
+            # StandardDescriptorNumbers.DEVICE: {},
+            # StandardDescriptorNumbers.CONFIGURATION: {},
+            # StandardDescriptorNumbers.STRING: {}
         }
         for type_number, index, raw_descriptor in self._descriptors:
+            if type_number not in descriptors:
+                descriptors[type_number] = {}
+
             descriptors[type_number][index] = raw_descriptor
+
+        self.maximum_type_number = sorted(descriptors.keys())[-1]
 
         # Check that indexes are continuous
         for type_number, indexes in sorted(descriptors.items()):
             assert(sorted(indexes.keys())[-1] == len(indexes) - 1)
 
         total_size = (# Base addresses
-                      16 +
+                      self.maximum_type_number * 4 +
                       # Points to data
                       functools.reduce(lambda x, indexes: x + len(indexes), descriptors.values(), 0) * 4 +
                       # Actual data + Length
@@ -113,16 +138,19 @@ class GetDescriptorHandlerSingleMemory(Elaboratable):
 
         # Fill ROM
 
-        # Write type offsets and number of entries
-        next_free_address = 0x10
-        type_index_base_address = [0, 0, 0, 0]
-        for type_number, indexes in sorted(descriptors.items()):
-            type_base_address = type_number*4
-            rom[type_base_address:type_base_address+4] = struct.pack(">HH", len(indexes), next_free_address)
-            type_index_base_address[type_number] = next_free_address
-            next_free_address += len(indexes)*4
+        # Fill are of type offsets with 0xFF, so that unused type numbers have a value of 0xFFFF
+        rom[0:(self.maximum_type_number+1)*4] = bytearray([0xFF] * (self.maximum_type_number+1)*4)
 
-        # # Write index offsets and data
+        # Write type offsets and number of entries
+        next_free_address = (self.maximum_type_number+1)*4
+        type_index_base_address = [0] * (self.maximum_type_number+1)
+        for type_number, indexes in sorted(descriptors.items()):
+            type_base_address = type_number * 4
+            rom[type_base_address:type_base_address + 4] = struct.pack(">HH", len(indexes), next_free_address)
+            type_index_base_address[type_number] = next_free_address
+            next_free_address += len(indexes) * 4
+
+        # Write index offsets and data
         for type_number, indexes in sorted(descriptors.items()):
             for index, raw_descriptor in sorted(indexes.items()):
                 index_base_address = type_index_base_address[type_number] + index * 4
@@ -134,7 +162,7 @@ class GetDescriptorHandlerSingleMemory(Elaboratable):
         #     data = " ".join([f"{rom[16*i+j]:02X}" for j in range(16 if i < (len(rom)//16) else  len(rom)-16*i)])
         #     print(f"{i*16:04X} {data}")
 
-        self.rom_content = [struct.unpack(">I", rom[4*i:4*i+4])[0] for i in range(total_size//4)]
+        self.rom_content = [struct.unpack(">I", rom[4*i:4*i+4])[0] for i in range(total_size//4+1)]
         self.descriptor_max_length = functools.reduce(lambda x, indexes: max(x, functools.reduce(lambda x, raw_descriptor: max(x, len(raw_descriptor)), indexes.values(), 0)), descriptors.values(), 0)
 
     def elaborate(self, platform) -> Module:
@@ -198,7 +226,7 @@ class GetDescriptorHandlerSingleMemory(Elaboratable):
                         position_in_stream.eq(self.start_position),
                     ]
 
-                    with m.If(length > 0):
+                    with m.If((length > 0) & (type_number <= self.maximum_type_number)):
                         m.next = 'TYPE'
                     with m.Else():
                         m.d.comb += [
@@ -206,8 +234,8 @@ class GetDescriptorHandlerSingleMemory(Elaboratable):
                         ]
 
             with m.State('TYPE'):
-                # If no entries are available for index, stall
-                with m.If(index >= rom_read_port.data.word_select(1, 16)):
+                # If no entries are available for index or type number is unused, stall
+                with m.If((index >= rom_read_port.data.word_select(1, 16)) | (rom_read_port.data == ~Const(0, 32))):
                     m.d.comb += [
                         self.stall.eq(1)
                     ]
@@ -235,7 +263,7 @@ class GetDescriptorHandlerSingleMemory(Elaboratable):
                     rom_read_port.addr.eq(descriptor_data_base_address+position_in_stream.bit_select(2, position_in_stream.width-2)),
                     self.tx.payload.eq(rom_read_port.data.word_select(3-position_in_stream.bit_select(0, 2), 8)),
 
-                    ## ... and base First and Last based on our current position in the stream.
+                    # ... and base First and Last based on our current position in the stream.
                     self.tx.first    .eq(on_first_packet),
                     self.tx.last     .eq(on_last_packet)
                 ]
@@ -293,6 +321,9 @@ class GetDescriptorHandlerSingleMemoryTest(LunaUSBGatewareTestCase):
                     e.bmAttributes     = 0x03
                     e.wMaxPacketSize   = 64
                     e.bInterval        = 11
+
+    # HID Descriptor (Example E.8 of HID specification)
+    descriptors.add_descriptor(b'\x09\x21\x01\x01\x00\x01\x22\x32')
 
     FRAGMENT_UNDER_TEST = GetDescriptorHandlerSingleMemory
     FRAGMENT_ARGUMENTS = {"descriptor_collection": descriptors}
@@ -386,20 +417,27 @@ class GetDescriptorHandlerSingleMemoryTest(LunaUSBGatewareTestCase):
                 yield from self._test_descriptor(type_number, index, raw_descriptor, 0, min(8, len(raw_descriptor)-1), delay_ready=10)
 
     @usb_domain_test_case
-    def test_all_descriptors_with_zero_length(self):
-        for type_number, index, raw_descriptor in self.descriptors:
-            yield from self._test_stall(type_number, index, 0, 0)
-
-    @usb_domain_test_case
     def test_all_descriptors_with_offset_and_length(self):
         for type_number, index, raw_descriptor in self.descriptors:
             if len(raw_descriptor) > 1:
                 yield from self._test_descriptor(type_number, index, raw_descriptor, 1, min(8, len(raw_descriptor)-1))
 
     @usb_domain_test_case
+    def test_all_descriptors_with_zero_length(self):
+        for type_number, index, raw_descriptor in self.descriptors:
+            yield from self._test_stall(type_number, index, 0, 0)
+
+    @usb_domain_test_case
     def test_unavailable_descriptor(self):
         yield from self._test_stall(StandardDescriptorNumbers.STRING, 100, 0, 64)
 
+    @usb_domain_test_case
+    def test_unavailable_index_type(self):
+        # Unavailable index in between
+        yield from self._test_stall(0x10, 0, 0, 64)
+
+        # Index after last used type
+        yield from self._test_stall(0x42, 0, 0, 64)
 
 if __name__ == "__main__":
     unittest.main()
