7011aa01aa76c957f711f71b59149668572c41e0
grvvy <jacob@greatscottgadgets.com>
2022-04-28 14:17:32 -0700

    enable debug mode in bash scripts

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 python3 -m venv testing-venv
 source testing-venv/bin/activate
 pip3 install poetry amaranth git+https://github.com/CapableRobot/CapableRobot_USBHub_Driver --upgrade
--- ci-scripts/test.sh
+++ ci-scripts/test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 usbhub --disable-i2c --hub D9D1 power state --port 3 --reset
 source testing-venv/bin/activate
 poetry run applets/interactive-test.py
