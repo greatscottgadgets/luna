a0b9762a0a8b76f83384802fdcd0eb0ea484d11c
grvvy <jacob@greatscottgadgets.com>
2022-05-05 10:22:37 -0600

    add wheel package to pip installation

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -4,6 +4,6 @@ env
 python3 -m venv testing-venv
 source testing-venv/bin/activate
 env
-pip3 install poetry amaranth git+https://github.com/CapableRobot/CapableRobot_USBHub_Driver --upgrade
+pip3 install wheel amaranth poetry git+https://github.com/CapableRobot/CapableRobot_USBHub_Driver --upgrade
 poetry install
 deactivate
