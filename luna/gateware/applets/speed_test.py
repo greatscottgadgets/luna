5c6b27af3538f2caff8907a65371ab894d5814d0
Martin Ling <martin-git@earth.li>
2023-05-15 11:29:51 +0100

    Make clock domain generation optional for speed test applets.

--- luna/gateware/applets/speed_test.py
+++ luna/gateware/applets/speed_test.py
@@ -15,7 +15,8 @@ BULK_ENDPOINT_NUMBER = 1
 class USBSpeedTestDevice(Elaboratable):
     """ Simple device that exchanges data with the host as fast as the hardware can. """
 
-    def __init__(self, fs_only=False, phy=None):
+    def __init__(self, generate_clocks=True, fs_only=False, phy=None):
+        self.generate_clocks = generate_clocks
         self.fs_only = fs_only
         self.phy = phy
         self.max_bulk_packet_size = 64 if fs_only else 512
@@ -66,7 +67,8 @@ class USBSpeedTestDevice(Elaboratable):
         m = Module()
 
         # Generate our domain clocks/resets.
-        m.submodules.car = platform.clock_domain_generator()
+        if self.generate_clocks:
+            m.submodules.clocks = platform.clock_domain_generator()
 
         # Request default PHY unless another was specified.
         if self.phy is None:
@@ -141,6 +143,9 @@ class USBInSuperSpeedTestDevice(Elaboratable):
 
     MAX_BULK_PACKET_SIZE = 1024
 
+    def __init__(self, generate_clocks=True):
+        self.generate_clocks = generate_clocks
+
     def create_descriptors(self):
         """ Create the descriptors we want to use for our device. """
 
@@ -186,8 +191,9 @@ class USBInSuperSpeedTestDevice(Elaboratable):
     def elaborate(self, platform):
         m = Module()
 
-        # Generate our domain clocks/resets.
-        m.submodules.car = platform.clock_domain_generator()
+        # Generate our clock domains, if needed.
+        if self.generate_clocks:
+            m.submodules.clocks = platform.clock_domain_generator()
 
         # Create our core PIPE PHY. Since PHY configuration is per-board, we'll just ask
         # our platform for a pre-configured USB3 PHY.
