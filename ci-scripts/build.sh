d1b1b28d2b46fd79e0951ba89265ea842eb8939d
grvvy <jacob@greatscottgadgets.com>
2024-03-13 16:50:19 -0600

    ci: move cynthion installation to jenkins script

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -3,4 +3,5 @@ set -e
 python3 -m venv testing-venv
 source testing-venv/bin/activate
 pip install .[dev]
+pip install "cynthion @ git+https://github.com/greatscottgadgets/cynthion/#subdirectory=cynthion/python/"
 deactivate
