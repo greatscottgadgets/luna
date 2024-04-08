6bdcc18796651861b7664f067725b4fb962ea27c
grvvy <jacob@greatscottgadgets.com>
2024-04-08 14:00:00 -0600

    ci: additionally install amaranth-stdio

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -4,4 +4,5 @@ python3 -m venv testing-venv
 source testing-venv/bin/activate
 pip install .[dev]
 pip install "cynthion @ git+https://github.com/greatscottgadgets/cynthion/#subdirectory=cynthion/python/"
+pip install "amaranth-stdio @ git+https://github.com/amaranth-lang/amaranth-stdio@4a14bb17"
 deactivate
