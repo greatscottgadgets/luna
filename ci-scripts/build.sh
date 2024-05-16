807a10053abbe7456b8b4cc863f4ed44c0c92e2d
Antoine van Gelder <antoine@greatscottgadgets.com>
2024-05-16 16:45:55 +0200

    ci: remove amaranth-stdio dep and add it via cynthion[gateware] instead

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -3,6 +3,5 @@ set -e
 python3 -m venv testing-venv
 source testing-venv/bin/activate
 pip install .[dev]
-pip install "cynthion @ git+https://github.com/greatscottgadgets/cynthion/#subdirectory=cynthion/python/"
-pip install "amaranth-stdio @ git+https://github.com/amaranth-lang/amaranth-stdio@4a14bb17"
+pip install "cynthion[gateware] @ git+https://github.com/greatscottgadgets/cynthion/#subdirectory=cynthion/python/"
 deactivate
