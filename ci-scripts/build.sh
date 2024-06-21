a42f3614733d92e3045776f05260d0b104a058d8
Mike Walters <mike@flomp.net>
2024-06-21 11:11:35 +0100

    test commit
    please ignore

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -2,6 +2,6 @@
 set -e
 python3 -m venv testing-venv
 source testing-venv/bin/activate
-pip install .[dev]
+pip install .
 pip install "cynthion[gateware] @ git+https://github.com/greatscottgadgets/cynthion/#subdirectory=cynthion/python/"
 deactivate
